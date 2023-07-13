f09b5d5000d1f1b15355c36604baffe94384877e
Mohit12S <96472482+Mohit12S@users.noreply.github.com>
2023-07-13 13:03:49 +0530

    Views Engine added and setup of views is done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 8000;
 // Using Express Router
 app.use('/' , require('./routes'));
 
+// Set Up Views Engine
+app.set('view engine' , 'ejs');
+app.set('views', './views');
 
 app.listen(port , function(err){
     if(err){
