8e283d87339bbd8c0eb6ebfd587eedbb79e5c3b7
Mohit12S <96472482+Mohit12S@users.noreply.github.com>
2023-07-21 20:28:15 +0530

    Setting Up Partials and Layouts

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,8 +1,14 @@
-module.exports.profile = function(req,res){
-    res.end('<h1>Users Profile</h1>');
-};
+// module.exports.profile = function(req,res){
+//     res.end('<h1>Users Profile</h1>');
+// };
 
 module.exports.likes = function(req,res){
     res.end('<h1>10 Likes</h1>');
+}
+
+module.exports.profile = function(req,res){
+    return res.render('user_profile',{
+        title : "Home"
+    });
 }
 
--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const app = express();
 
 const port = 8000;
 
+// Using Express Layouts
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
+// Extract style and scripts from subpages into the layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 // Using Express Router
 app.use('/' , require('./routes'));
+
+// Accessing Static files
+app.use(express.static('./assets'));
+
+
 
 // Set Up Views Engine
 app.set('view engine' , 'ejs');
